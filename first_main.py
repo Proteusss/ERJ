f5832a85620f3418b3a9c65a4f8b41c1ad716bfe
Proteusss <jingbzh@gmail.com>
2019-03-27 21:26:25 +0800

    UI_change_&_Signal_add

--- first_main.py
+++ first_main.py
@@ -2,12 +2,14 @@ import sys,cv2,time
 from first_ui import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog,QWidget,QMainWindow ,QGraphicsScene
-from PyQt5.QtCore import QTimer, QThread, pyqtSignal, Qt,QDateTime
+from PyQt5.QtCore import QTimer, QThread, pyqtSignal, Qt,QDateTime,qDebug
 from PyQt5.QtGui import  QPixmap, QImage
 from PyQt5.QtWidgets import QPushButton,QLabel
 import numpy as np
 import qdarkstyle
 import string
+import os
+
 
 class mywindow(QMainWindow,Ui_MainWindow): #这个窗口继承了用QtDesignner 绘制的窗口
 
@@ -22,6 +24,25 @@ class mywindow(QMainWindow,Ui_MainWindow): #这个窗口继承了用QtDesignner 
         self.video1_graphicsView.setScene(scene)
         #setPixmap(QPixmap.fromImage(image))
 
+    def setImage2(self, image):
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap.fromImage(image))
+        #self.video1_graphicsView.fitInView(scene)
+        self.video2_graphicsView.setScene(scene)
+        #setPixmap(QPixmap.fromImage(image))
+    def setImage3(self, image):
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap.fromImage(image))
+        #self.video1_graphicsView.fitInView(scene)
+        self.video3_graphicsView.setScene(scene)
+        #setPixmap(QPixmap.fromImage(image))
+    def setImage4(self, image):
+        scene = QGraphicsScene()
+        scene.addPixmap(QPixmap.fromImage(image))
+        #self.video1_graphicsView.fitInView(scene)
+        self.video4_graphicsView.setScene(scene)
+        #setPixmap(QPixmap.fromImage(image))
+
     def setList(self, tp):
         self.alertlistView.addItem(str(tp))
 
@@ -33,13 +54,56 @@ class mywindow(QMainWindow,Ui_MainWindow): #这个窗口继承了用QtDesignner 
                                                            "打开视频",
                                                            "",
                                                            # " *.jpg;;*.png;;*.jpeg;;*.bmp")
-                                                           " *.mp4;;*.avi;;All Files (*)")
+                                                           " *.mov;;*.mp4;;*.avi;;All Files (*)")
         # cap = cv2.VideoCapture(str(videoName))
         th = Thread(self)
         th.changePixmap.connect(self.setImage)
-        th.changeList.connect(self.setList)
+        #th.changeList.connect(self.setList)
         th.start()
+    def video2processing(self):
+        print("gogo")
 
+        global videoName  # 在这里设置全局变量以便在线程中使用
+        videoName, videoType = QFileDialog.getOpenFileName(self,
+                                                           "打开视频",
+                                                           "",
+                                                           # " *.jpg;;*.png;;*.jpeg;;*.bmp")
+                                                           " *.mov;;*.mp4;;*.avi;;All Files (*)")
+        # cap = cv2.VideoCapture(str(videoName))
+        th2 = Thread(self)
+        th2.changePixmap.connect(self.setImage2)
+        #th2.changeList.connect(self.setList)
+        th2.start()
+
+    def video3processing(self):
+        print("gogo")
+
+        global videoName  # 在这里设置全局变量以便在线程中使用
+        videoName, videoType = QFileDialog.getOpenFileName(self,
+                                                           "打开视频",
+                                                           "",
+                                                           # " *.jpg;;*.png;;*.jpeg;;*.bmp")
+                                                           " *.mov;;*.mp4;;*.avi;;All Files (*)")
+        # cap = cv2.VideoCapture(str(videoName))
+        th3 = Thread(self)
+        th3.changePixmap.connect(self.setImage3)
+        #th3.changeList.connect(self.setList)
+        th3.start()
+
+    def video4processing(self):
+        print("gogo")
+
+        global videoName  # 在这里设置全局变量以便在线程中使用
+        videoName, videoType = QFileDialog.getOpenFileName(self,
+                                                           "打开视频",
+                                                           "",
+                                                           # " *.jpg;;*.png;;*.jpeg;;*.bmp")
+                                                           " *.mov;;*.mp4;;*.avi;;All Files (*)")
+        # cap = cv2.VideoCapture(str(videoName))
+        th4 = Thread(self)
+        th4.changePixmap.connect(self.setImage4)
+        #th4.changeList.connect(self.setList)
+        th4.start()
 
 class Thread(QThread):  # 采用线程来播放视频
 
@@ -48,12 +112,14 @@ class Thread(QThread):  # 采用线程来播放视频
 
     def run(self):
         cap = cv2.VideoCapture(videoName)
-
+        qDebug('Worker.on_timeout get called from: %s' % hex(int(QThread.currentThreadId())))
         print(videoName)
         while (cap.isOpened() == True):
             ret, frame = cap.read()
+            name_part = 0
             if ret:
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                #cv2.imwrite(os.curdir()+'/out/'+videoName+'_'+str(name_part)+'.jpg', rgbImage)
                 time2 = QDateTime.currentDateTimeUtc().toString()
                 # print(np.array(rgbImage).shape)
                 mes = time2 + str(np.array(rgbImage).shape)
@@ -62,7 +128,8 @@ class Thread(QThread):  # 采用线程来播放视频
                                                  QImage.Format_RGB888)  # 在这里可以对每帧图像进行处理，
                 p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
                 self.changePixmap.emit(p)
-                time.sleep(0.01)  # 控制视频播放的速度
+                time.sleep(0.014)  # 控制视频播放的速度
+                name_part += 1
             else:
                 break
 
@@ -71,6 +138,7 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    qDebug('From main thread: %s' % hex(int(QThread.currentThreadId())))
 
     window = mywindow()
     window.setWindowState(QtCore.Qt.WindowMaximized)
